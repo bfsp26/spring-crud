28cfcf44e0ffdaf8fd5628c33b2f4fffac6c4819
Bryan Simbaña <bryan-_1997@outlook.com>
2022-06-29 14:18:35 -0500

    Create new person

--- src/main/java/ec/tcs/springcrud/controllers/PersonController.java
+++ src/main/java/ec/tcs/springcrud/controllers/PersonController.java
@@ -1,11 +1,13 @@
 package ec.tcs.springcrud.controllers;
 
+import ec.tcs.springcrud.entities.Person;
 import ec.tcs.springcrud.services.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -14,11 +16,23 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping(path = "/people")
+    @GetMapping(path = "/")
     public String getPeople(Model model) {
         var people = personService.listPeople();
         log.info("Run controller");
         model.addAttribute("people", people);
         return "index";
     }
+
+    @GetMapping(path = "/add")
+    public String add(Person person) {
+        return "update";
+    }
+
+    @PostMapping(path = "/save")
+    public String save(Person person) {
+        personService.save(person);
+        return "redirect:/";
+    }
+
 }
